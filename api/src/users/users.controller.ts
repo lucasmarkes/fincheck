8ab7077c822f6022a182963da68fd71e09be0ba5
Lucas Marques <luandrealbuquerque@gmail.com>
2024-11-18 20:05:25 -0300

    feat: add class-transformer and class-validator dependencies, and implement validation in CreateUserDto

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import {
+	Body,
+	Controller,
+	Post,
+	UsePipes,
+	ValidationPipe,
+} from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UsersService } from "./users.service";
 
@@ -7,6 +13,7 @@ export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
 	@Post()
+	@UsePipes(new ValidationPipe())
 	create(@Body() createUserDto: CreateUserDto) {
 		return this.usersService.create(createUserDto);
 	}
