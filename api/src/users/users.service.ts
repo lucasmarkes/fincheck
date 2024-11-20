ebbc548e429a72d9a7fc4a4cb1aeb1a4986b4e22
Lucas Marques <luandrealbuquerque@gmail.com>
2024-11-19 22:18:55 -0300

    feat: implement PrismaService for database connection and update UsersService to use Prisma for user creation

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/database/prisma.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
 export class UsersService {
-	create(createUserDto: CreateUserDto) {
-		return createUserDto;
+	constructor(private readonly prismaService: PrismaService) {}
+
+	async create(createUserDto: CreateUserDto) {
+		const user = await this.prismaService.user.create({
+			data: {
+				email: createUserDto.email,
+				name: createUserDto.name,
+				password: createUserDto.password,
+			},
+		});
+
+		return user;
 	}
 }
