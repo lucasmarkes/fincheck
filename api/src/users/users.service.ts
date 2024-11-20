262f85d31299e8b96c76cc048e600b6571ddc11c
Lucas Marques <luandrealbuquerque@gmail.com>
2024-11-19 22:36:26 -0300

    feat: add bcryptjs for password hashing and implement email conflict check in UsersService

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable } from "@nestjs/common";
+import { hash } from "bcryptjs";
 import { PrismaService } from "src/database/prisma.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 
@@ -7,12 +8,20 @@ export class UsersService {
 	constructor(private readonly prismaService: PrismaService) {}
 
 	async create(createUserDto: CreateUserDto) {
+		const { email, name, password } = createUserDto;
+
+		const emailExists = await this.prismaService.user.findUnique({
+			where: { email },
+		});
+
+		if (emailExists) {
+			throw new ConflictException("This email is already in use.");
+		}
+
+		const hashedPassword = await hash(password, 10);
+
 		const user = await this.prismaService.user.create({
-			data: {
-				email: createUserDto.email,
-				name: createUserDto.name,
-				password: createUserDto.password,
-			},
+			data: { email, name, password: hashedPassword },
 		});
 
 		return user;
