da33d4654a2f884cea334b27fb5c3ce3515d1513
Lucas Marques <luandrealbuquerque@gmail.com>
2024-12-01 08:07:04 -0300

    feat: implement signup and signin functionality with DTO refactoring in AuthModule

--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import {
+	ConflictException,
+	Injectable,
+	UnauthorizedException,
+} from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
-import { compare } from "bcryptjs";
+import { compare, hash } from "bcryptjs";
 import { UsersRepository } from "src/shared/database/repositories/users.repositories";
-import { AuthenticateDto } from "./dto/authenticate.dto";
+import { SignInDto } from "./dto/signin.dto";
+import { SignUpDto } from "./dto/signup.dto";
 
 @Injectable()
 export class AuthService {
@@ -11,8 +16,8 @@ export class AuthService {
 		private readonly jwtService: JwtService,
 	) {}
 
-	async authenticate(authenticateDto: AuthenticateDto) {
-		const { email, password } = authenticateDto;
+	async authenticate(signInDto: SignInDto) {
+		const { email, password } = signInDto;
 
 		const user = await this.usersRepo.findUnique({
 			where: {
@@ -31,8 +36,60 @@ export class AuthService {
 		}
 
 		// Generate JWT token
-		const accessToken = this.jwtService.sign({ sub: user.id });
+		const accessToken = await this.generateAccessToken(user.id);
 
 		return { accessToken };
+	}
+
+	async signup(signUpDto: SignUpDto) {
+		const { email, name, password } = signUpDto;
+
+		const emailExists = await this.usersRepo.findUnique({
+			where: { email },
+			select: { id: true },
+		});
+
+		if (emailExists) {
+			throw new ConflictException("This email is already in use.");
+		}
+
+		const hashedPassword = await hash(password, 10);
+
+		const user = await this.usersRepo.create({
+			data: {
+				email,
+				name,
+				password: hashedPassword,
+				categories: {
+					createMany: {
+						data: [
+							// Income
+							{ name: "Salário", icon: "salary", type: "INCOME" },
+							{ name: "Freelance", icon: "freelance", type: "INCOME" },
+							{ name: "Outro", icon: "other", type: "INCOME" },
+							// Expense
+							{ name: "Casa", icon: "home", type: "EXPENSE" },
+							{ name: "Alimentação", icon: "food", type: "EXPENSE" },
+							{ name: "Educação", icon: "education", type: "EXPENSE" },
+							{ name: "Lazer", icon: "fun", type: "EXPENSE" },
+							{ name: "Mercado", icon: "grocery", type: "EXPENSE" },
+							{ name: "Roupas", icon: "clothes", type: "EXPENSE" },
+							{ name: "Transporte", icon: "transport", type: "EXPENSE" },
+							{ name: "Viagem", icon: "travel", type: "EXPENSE" },
+							{ name: "Outro", icon: "other", type: "EXPENSE" },
+						],
+					},
+				},
+			},
+		});
+
+		const accessToken = await this.generateAccessToken(user.id);
+
+		return { accessToken };
+	}
+
+	private async generateAccessToken(userId: string) {
+		const payload = { sub: userId };
+		return this.jwtService.sign(payload);
 	}
 }
--- api/src/modules/auth/dto/signin.dto.ts
+++ api/src/modules/auth/dto/signin.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
-export class AuthenticateDto {
+export class SignInDto {
 	@IsString()
 	@IsNotEmpty()
 	@IsEmail()
