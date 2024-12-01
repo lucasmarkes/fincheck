9f375e2de996f7625cb73cb49071aeac188a75d1
Lucas Marques <luandrealbuquerque@gmail.com>
2024-12-01 07:48:51 -0300

    feat: add dotenv configuration for environment variables and refactor AuthModule to use new env structure

--- api/src/modules/auth/auth.module.ts
+++ api/src/modules/auth/auth.module.ts
@@ -1,17 +1,17 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
-import { env } from "node:process";
+import { env } from "src/shared/config/env";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
 @Module({
-	controllers: [AuthController],
-	providers: [AuthService],
 	imports: [
 		JwtModule.register({
 			signOptions: { expiresIn: "1d" },
-			secret: env.JWT_SECRET,
+			secret: env.jwtSecret,
 		}),
 	],
+	controllers: [AuthController],
+	providers: [AuthService],
 })
 export class AuthModule {}
