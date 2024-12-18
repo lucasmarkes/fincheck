ae8ac9898e53d01571163570746a87b52f982d54
Lucas Marques <luandrealbuquerque@gmail.com>
2024-12-18 19:53:05 -0300

    feat: refactor BankAccounts module to improve service structure and add ownership validation

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from "@nestjs/common";
 import { APP_GUARD } from "@nestjs/core";
-
 import { AuthGuard } from "./modules/auth/auth.guard";
 import { AuthModule } from "./modules/auth/auth.module";
+import { BankAccountsModule } from "./modules/bank-accounts/bank-accounts.module";
+import { CategoriesModule } from "./modules/categories/categories.module";
 import { UsersModule } from "./modules/users/users.module";
 import { DatabaseModule } from "./shared/database/database.module";
-import { CategoriesModule } from './modules/categories/categories.module';
-import { BankAccountsModule } from './modules/bank-accounts/bank-accounts.module';
 
 @Module({
-	imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule, BankAccountsModule],
+	imports: [
+		UsersModule,
+		DatabaseModule,
+		AuthModule,
+		CategoriesModule,
+		BankAccountsModule,
+	],
 	controllers: [],
 	providers: [
 		{
