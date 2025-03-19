7e7ac0a995b853ffd6533e39b3f1d5fba3046d85
Lucas Marques <luandrealbuquerque@gmail.com>
2025-03-19 14:58:23 -0300

    feat: enhance Button and Input components to support additional class names for styling

--- fe/src/view/components/Button.tsx
+++ fe/src/view/components/Button.tsx
@@ -1,16 +1,20 @@
 import type { ComponentProps } from "react";
+import { cn } from "../../utils/cn";
 
 interface ButtonProps extends ComponentProps<"button"> {
 	type?: "submit" | "button" | "reset";
 	label: string;
 }
 
-export function Button({ type, label, ...props }: ButtonProps) {
+export function Button({ type, label, className, ...props }: ButtonProps) {
 	return (
 		<button
-			type={type}
 			{...props}
-			className="bg-teal-800 hover:bg-teal-900 disabled:bg-gray-400 disabled:cursor-not-allowed px-6 h-12 rounded-2xl font-medium text-white transition-all hover:cursor-pointer"
+			type={type}
+			className={cn(
+				"bg-teal-800 hover:bg-teal-900 disabled:bg-gray-400 disabled:cursor-not-allowed px-6 h-12 rounded-2xl font-medium text-white transition-all hover:cursor-pointer",
+				className,
+			)}
 		>
 			{label}
 		</button>
--- fe/src/view/components/Input.tsx
+++ fe/src/view/components/Input.tsx
@@ -7,7 +7,7 @@ interface InputProps extends ComponentProps<"input"> {
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-	({ placeholder, name, id, error, ...props }, ref) => {
+	({ placeholder, name, id, error, className, ...props }, ref) => {
 		const inputID = id || name;
 
 		return (
@@ -20,6 +20,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
 					className={cn(
 						"bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800 pt-4 peer placeholder-shown:pt-0 focus:border-gray-800 transition-all outline-none",
 						error && "!border-red-900",
+						className,
 					)}
 					placeholder=" "
 				/>
