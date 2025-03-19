a8fd7b7771d168b3e427f683ff65a1e85a1d8c4f
Lucas Marques <luandrealbuquerque@gmail.com>
2025-03-19 03:17:24 -0300

    feat: update Button component styles for improved hover effect

--- fe/src/view/components/Button.tsx
+++ fe/src/view/components/Button.tsx
@@ -10,7 +10,7 @@ export function Button({ type, label, ...props }: ButtonProps) {
 		<button
 			type={type}
 			{...props}
-			className="bg-teal-900 hover:bg-teal-800 disabled:bg-gray-400 disabled:cursor-not-allowed px-6 h-12 rounded-2xl font-medium text-white transition-all"
+			className="bg-teal-800 hover:bg-teal-900 disabled:bg-gray-400 disabled:cursor-not-allowed px-6 h-12 rounded-2xl font-medium text-white transition-all hover:cursor-pointer"
 		>
 			{label}
 		</button>
