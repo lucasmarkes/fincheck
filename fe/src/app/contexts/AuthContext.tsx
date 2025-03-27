00d2c633a9d64246d4cab7c30d40526b76e3e8ca
Lucas Marques <luandrealbuquerque@gmail.com>
2025-03-27 00:34:24 -0300

    feat: implement user authentication with signout functionality and integrate user data fetching

--- fe/src/app/contexts/AuthContext.tsx
+++ fe/src/app/contexts/AuthContext.tsx
@@ -1,9 +1,11 @@
 import { createContext, useCallback, useState } from "react";
 import { localStorageKeys } from "../config/localStorageKeys";
-
+import { useQuery } from "@tanstack/react-query";
+import { usersService } from "../services/usersService";
 interface AuthContextValue {
 	signedIn: boolean;
 	signin: (accessToken: string) => void;
+	signout: () => void;
 }
 
 export const AuthContext = createContext({} as AuthContextValue);
@@ -17,14 +19,24 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 		return !!storedAccessToken;
 	});
 
+	useQuery({
+		queryKey: ["users", "me"],
+		queryFn: () => usersService.me(),
+	});
+
 	const signin = useCallback((accessToken: string) => {
 		localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
 
 		setSignedIn(true);
 	}, []);
 
+	const signout = useCallback(() => {
+		localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
+		setSignedIn(false);
+	}, []);
+
 	return (
-		<AuthContext.Provider value={{ signedIn, signin }}>
+		<AuthContext.Provider value={{ signedIn, signin, signout }}>
 			{children}
 		</AuthContext.Provider>
 	);
