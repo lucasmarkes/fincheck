0b2486296c457b497aea3f9af62a2dc7a288d14f
Lucas Marques <luandrealbuquerque@gmail.com>
2025-03-28 16:22:49 -0300

    feat: enhance Loader component with logo and updated styling for better visibility

--- fe/src/view/components/Loader.tsx
+++ fe/src/view/components/Loader.tsx
@@ -1,9 +1,15 @@
+import { Logo } from "./Logo";
 import { Spinner } from "./Spinner";
 
 export function Loader() {
 	return (
-		<div className="fixed top-0 left-0 w-full h-full bg-gray-50 flex justify-center items-center">
-			<Spinner />
+		<div className="fixed top-0 left-0 w-full h-full bg-teal-900 flex justify-center items-center">
+			<div className="flex flex-col items-center gap-4">
+				<div className="animate-pulse">
+					<Logo className="h-10 text-white" />
+				</div>
+				<Spinner className="text-teal-900 fill-white" />
+			</div>
 		</div>
 	);
 }
